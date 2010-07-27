c4b7c0d8a0cc5fd7f43257594bd0a71c7bd12a63
Marak Squires <marak.squires@gmail.com>
2010-07-27 16:05:00 -0400

    updating demo

--- demo.js
+++ demo.js
@@ -8,7 +8,7 @@
  
 var vows = require('vows'),
     sys = require('sys'),
-    colors = require('./vendor/colors')
+    colors = require('colors')
     assert = require('assert'),
     http = require('http');
 
@@ -16,40 +16,40 @@ var HttpProxy = require('./lib/node-http-proxy').HttpProxy;
 var testServers = {};
 
 
-// regular http server
-http.createServer(function (req, res){
-  // Initialize the nodeProxy and start proxying the request
-  var proxy = new (HttpProxy);
-  proxy.init(req, res);
-  // lets proxy the request to another service
-  proxy.proxyRequest('localhost', '8081', req, res);
-  
-}).listen(8080);
-sys.puts('started a http server on port 8080'.green)
+// ascii art from http://github.com/marak/asciimo
+var welcome = '\
+#    # ##### ##### #####        #####  #####   ####  #    # #   # \n\
+#    #   #     #   #    #       #    # #    # #    #  #  #   # #  \n\
+######   #     #   #    # ##### #    # #    # #    #   ##     #   \n\
+#    #   #     #   #####        #####  #####  #    #   ##     #   \n\
+#    #   #     #   #            #      #   #  #    #  #  #    #   \n\
+#    #   #     #   #            #      #    #  ####  #    #   #   \n';
+sys.puts(welcome.rainbow.bold);
 
-// http server with latency
+// create regular http proxy server
 http.createServer(function (req, res){
-  // Initialize the nodeProxy and start proxying the request
   var proxy = new (HttpProxy);
   proxy.init(req, res);
-  
-  // lets proxy the request to another service
+  proxy.proxyRequest('localhost', '9000', req, res);
+}).listen(8000);
+sys.puts('http proxy server'.blue + ' started '.green.bold + 'on port '.blue + '8000'.yellow);
+
+// http proxy server with latency
+http.createServer(function (req, res){
+  var proxy = new (HttpProxy);
+  proxy.init(req, res);
   setTimeout(function(){
-    proxy.proxyRequest('localhost', '8090', req, res);
+    proxy.proxyRequest('localhost', '9000', req, res);
   }, 200)
-  
-}).listen(8081);
-sys.puts('started a http server with latency on port 8081'.green)
+}).listen(8001);
+sys.puts('http proxy server '.blue + 'started '.green.bold + 'on port '.blue + '8001 '.yellow + 'with latency'.magenta.underline );
 
-
-
+// create regular http server 
 http.createServer(function (req, res){
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.write('foo');
   res.end();
-}).listen(8090);
-sys.puts('started another http server on port 8090'.green)
-
-
-sys.puts('to test the proxy server, request http://localhost:8080/ in your browser.');
-sys.puts('your request will proxy to the server running on port 8081');
+}).listen(9000);
+sys.puts('http server '.blue + 'started '.green.bold + 'on port '.blue + '9000 '.yellow);
+//sys.puts('to test the proxy server, request http://localhost:8080/ in your browser.');
+//sys.puts('your request will proxy to the server running on port 8081');
