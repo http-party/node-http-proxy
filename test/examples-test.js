c4a7b1584302fe12a8fc06b6774db5ff602c3607
indexzero <charlie.robbins@gmail.com>
2012-07-26 04:47:25 -0400

    [fix] Suppress EADDRINUSE errors from `test/examples-test.js` since we are just looking for require-time errors. Isolate tests to ensure idempotency of ports

--- test/examples-test.js
+++ test/examples-test.js
@@ -9,6 +9,16 @@ var vows = require('vows')
     macros = require('./macros'),
     examples = macros.examples;
 
+//
+// Suppress `EADDRINUSE` errors since
+// we are just checking for require-time errors
+//
+process.on('uncaughtException', function (err) {
+  if (err.code !== 'EADDRINUSE') {
+    throw err;
+  }
+});
+
 vows.describe('node-http-proxy/examples').addBatch(
   examples.shouldHaveDeps()
 ).addBatch(
