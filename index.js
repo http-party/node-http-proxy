8273cb6461e4d33f36e583b0354d1bea038d0a56
yawnt <yawn.localhost@gmail.com>
2013-08-03 08:47:07 +0200

    [refactor] move to leaner architecture

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+/*!
+ *
+ * Charon the demon, with the eyes of glede,
+ * Beckoning to them, collects them all together,
+ * Beats with his oar whoever lags behind
+ *          Dante - The Divine Comedy (Canto III)
+ *
+ */
+
 module.exports = require('./lib/caronte');
