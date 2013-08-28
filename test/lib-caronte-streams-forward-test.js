8fc33893672d26013c2b2ff396b777bcf1751527
yawnt <yawn.localhost@gmail.com>
2013-08-28 14:49:27 +0200

    [test] add test for forwardstream

--- test/lib-caronte-streams-forward-test.js
+++ test/lib-caronte-streams-forward-test.js
@@ -1,4 +1,5 @@
-var ForwardStream = require('../lib/caronte/streams/forward'),
+var caronte       = require('../'),
+    ForwardStream = require('../lib/caronte/streams/forward');
     expect        = require('expect.js'),
     Writable      = require('stream').Writable,
     http          = require('http');
@@ -22,16 +23,23 @@ describe('lib/caronte/passes/web.js', function () {
   });
 
   describe('should pipe the request and finish it', function () {
-    it('should make the request on pipe and finish it');
-    var stubOptions = {
-      target: {
-        hostname  : 'www.google.com',
-        port      : '80',
-        path       : '/'
-      }
-    };
+    it('should make the request on pipe and finish it', function(done) {
+      var result;
+      
+      var p = caronte.createProxyServer({
+        forward: 'http://127.0.0.1:8080'
+      }).listen('8081')
 
-    var forwardProxy = new ForwardStream({});
+      var s = http.createServer(function(req, res) {
+        expect(req.method).to.eql('GET');
+        s.close();
+        p.close();
+        done();
+      });
 
+      s.listen('8080');
+      
+      http.request('http://127.0.0.1:8081', function() {}).end();
+    });
   });
 });
