a467b7b4a9614a7cbfdc256524e1495616e3d4d9
cronopio <aristizabal.daniel@gmail.com>
2013-11-07 17:20:28 -0500

    [examples] fixed https examples

--- examples/https.js
+++ examples/https.js
@@ -1,8 +1,10 @@
-var httpProxy = require('http-proxy');
+var httpProxy = require('../lib/http-proxy');
 /*
  * Create your proxy server pointing to a secure domain
  */
-var options = {target:'https://google.com'};
+var options = { target:'https://google.com',
+                headers: {host: 'google.com'}
+              };
 
 var proxyServer = httpProxy.createProxyServer(options);
 console.log("Proxy server listening on port 8000");
