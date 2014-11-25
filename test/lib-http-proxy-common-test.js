70ed1c4273bc64500e8bae9b60d7fd6a19135246
koolc <koolc@users.noreply.github.com>
2014-11-25 17:50:36 +0800

    [Bugfix] Allow for multiple ? in outgoing urls.
    Without this fix urls that had multiple ? in them would drop sections
    of the url since before there was an assumption of there only being one.

--- test/lib-http-proxy-common-test.js
+++ test/lib-http-proxy-common-test.js
@@ -203,13 +203,13 @@ describe('lib/http-proxy/common.js', function () {
       expect(outgoing.path).to.eql('/forward/static/path');
     })
 
-    it('should not modify the query string', function () {
+    it.only('should not modify the query string', function () {
       var outgoing = {};
       common.setupOutgoing(outgoing, {
         target: { path: '/forward' },
-      }, { url: '/?foo=bar//&target=http://foobar.com/' });
+      }, { url: '/?foo=bar//&target=http://foobar.com/?a=1%26b=2&other=2' });
 
-      expect(outgoing.path).to.eql('/forward/?foo=bar//&target=http://foobar.com/');
+      expect(outgoing.path).to.eql('/forward/?foo=bar//&target=http://foobar.com/?a=1%26b=2&other=2');
     })
   });
 
