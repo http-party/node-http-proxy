c3f2728844225b9600daca54efc11ad548367434
Tyr Chen <tyr.chen@gmail.com>
2015-08-29 09:19:35 -0700

    fix #873. Silently drop the error to let the benchmark finish.

--- benchmark/scripts/proxy.js
+++ benchmark/scripts/proxy.js
@@ -3,4 +3,12 @@ var http = require('http'),
 //
 // Create your proxy server
 //
-httpProxy.createProxyServer({ target: 'http://localhost:9000' }).listen(8000);
+httpProxy.createProxyServer({ target: 'http://localhost:9000' })
+  .on('error', function(e) {
+    // Silently drop the errors without terminate the proxy.
+    // read / connection timeout errors are likely occur when performing
+    // "wrk -c 100 -d5m -t 8 http://127.0.0.1:8000". wrk will accumualate
+    // the errors.
+    // console.log(e);
+  })
+  .listen(8000);
