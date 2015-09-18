626cba7685c2ff5e3a7ca42ebe66821f04c53642
Martinez, Andrew <andrew.martinez@calero.com>
2015-09-18 14:13:43 -0400

    resolves #882
    Adds a proxyWsReq event that can be used to modify the outgoing
    websocket handshake.

--- test/lib-http-proxy-passes-ws-incoming-test.js
+++ test/lib-http-proxy-passes-ws-incoming-test.js
@@ -1,5 +1,7 @@
 var httpProxy = require('../lib/http-proxy/passes/ws-incoming'),
-    expect = require('expect.js');
+    expect = require('expect.js'),
+    http = require('http'),
+    httpProxyLib = require('../lib/http-proxy');
 
 describe('lib/http-proxy/passes/ws-incoming.js', function () {
   describe('#checkMethodAndHeader', function () {
@@ -9,7 +11,7 @@ describe('lib/http-proxy/passes/ws-incoming.js', function () {
         method: 'DELETE',
         headers: {}
       },
-      stubSocket = { 
+      stubSocket = {
         destroy: function () {
           // Simulate Socket.destroy() method when call
           destroyCalled = true;
@@ -74,6 +76,62 @@ describe('lib/http-proxy/passes/ws-incoming.js', function () {
       expect(returnValue).to.be(undefined);
       expect(destroyCalled).to.be(false);
     })
+
+
+    it('should detect a proxyWsReq event and modify headers', function (done) {
+      var proxy = httpProxyLib.createProxyServer({
+        target: 'http://127.0.0.1:8080',
+        ws: true
+      });
+
+      proxy.on('proxyWsReq', function(proxyWsReq, req, res, options) {
+        proxyWsReq.setHeader('X-Special-Proxy-Header', 'foobar');
+      });
+
+      function requestHandler(req, res) {
+        proxy.web(req, res);
+      }
+
+      var proxyServer = http.createServer(requestHandler);
+
+      proxyServer.on('upgrade', function(req, res, head){
+          proxy.ws(req, res, head);
+      });
+
+      var source = http.createServer(function(req, res) {
+          res.end();
+      });
+
+      source.on('upgrade', function(req, res, head){
+          expect(req.headers['x-special-proxy-header']).to.eql('foobar');
+          source.close();
+          proxyServer.close();
+          done();
+      });
+
+      source.on('error', function(){});
+      proxy.on('error', function(){});
+      proxyServer.on('error', function(){});
+
+      proxyServer.listen('8081');
+      source.listen('8080');
+
+      var request = http.request({
+        host: '127.0.0.1',
+        port: '8081',
+        method: 'GET',
+        path: '/',
+        headers: {
+            'upgrade': 'websocket',
+            'connection': 'Upgrade',
+            'sec-websocket-key': 'dGhlIHNhbXBsZSBub25jZQ==',
+            'sec-websocket-protocol': 'chat, superchat',
+            'sec-websocket-version': 13
+        }
+      }, function() {});
+      request.on('error', function(){});
+      request.end();
+    });
   });
 
   describe('#XHeaders', function () {
