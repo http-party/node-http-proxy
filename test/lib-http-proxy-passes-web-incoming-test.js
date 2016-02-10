8a60ca43fb554795acf0243f0902cb599d54a942
Jarrett Cruger <jcrugzz@gmail.com>
2016-02-09 19:11:25 -0500

    [fix] pass more useful information to the error that we dont already have access to

--- test/lib-http-proxy-passes-web-incoming-test.js
+++ test/lib-http-proxy-passes-web-incoming-test.js
@@ -144,11 +144,9 @@ describe('#createProxyServer.web() using own http server', function () {
     var proxyServer = http.createServer(requestHandler);
 
     function requestHandler(req, res) {
-      proxy.once('error', function (err, errReq, errRes) {
+      proxy.once('error', function (err) {
         proxyServer.close();
         expect(err).to.be.an(Error);
-        expect(errReq).to.be.equal(req);
-        expect(errRes).to.be.equal(res);
         expect(err.code).to.be('ECONNREFUSED');
         done();
       });
@@ -177,11 +175,9 @@ describe('#createProxyServer.web() using own http server', function () {
 
     var started = new Date().getTime();
     function requestHandler(req, res) {
-      proxy.once('error', function (err, errReq, errRes) {
+      proxy.once('error', function (err) {
         proxyServer.close();
         expect(err).to.be.an(Error);
-        expect(errReq).to.be.equal(req);
-        expect(errRes).to.be.equal(res);
         expect(new Date().getTime() - started).to.be.greaterThan(99);
         expect(err.code).to.be('ECONNRESET');
         done();
@@ -217,11 +213,9 @@ describe('#createProxyServer.web() using own http server', function () {
 
     var started = new Date().getTime();
     function requestHandler(req, res) {
-      proxy.once('error', function (err, errReq, errRes) {
+      proxy.once('error', function (err) {
         proxyServer.close();
         expect(err).to.be.an(Error);
-        expect(errReq).to.be.equal(req);
-        expect(errRes).to.be.equal(res);
         expect(err.code).to.be('ECONNRESET');
         doneOne();
       });
